88253f957582ee7fa1fde7a6322c4c22bb7d9a8a
hpcslag <cslag@hotmail.com.tw>
2014-10-18 00:09:15 +0800

    Version 04 Test

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ var appone = new Serv({
   baseDir: path.join(__dirname,'./www'), //Setup base Directory!
   dirindex:['index.html','default.html'] //Setup Your default index, like apache HTTPD.conf file!
 });
-appone.listen(80,'127.0.0.1'); //Setup Port, IP Address!
+appone.listen(81,'127.0.0.1'); //Setup Port, IP Address!
