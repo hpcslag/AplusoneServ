1c6d92591c94a7621f271a0301144323c701b85c
unknown <cslag@hotmail.com.tw>
2014-10-21 00:08:16 +0800

    Debug ./ and Custom configure.json

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
 var Serv = require('./lib/Serv');
 var path = require('path');
+var fs = require('fs');
+var colors = require('colors');
 
-var appone = new Serv({
-  baseDir: path.join(__dirname,'./www'), //Setup base Directory!
-  dirindex:['index.html','default.html'] //Setup Your default index, like apache HTTPD.conf file!
+
+fs.readFile('configure.json',function(err,data){
+	if(err){
+		console.log("["+"ERROR!".red+"] Not Found Connfigure.json File, Please Create New Configure.json File!");
+		process.exit();
+	}else{
+		var conf = JSON.parse(data);
+
+		var appone = new Serv({
+			baseDir: path.join(__dirname,conf.baseDir),
+			dirindex: conf.dirindex
+		});
+		appone.listen(conf.host.port,conf.host.ip); //Setup Port, IP Address!
+		console.log("["+"RUN".green+"] Server Run as "+conf.host.ip+":"+conf.host.port);
+		console.log("	                    ˍ▂▃▄▄".red);
+		console.log("                 	◢████████◤".red);
+		console.log("           ◢        ██████████▆▄".red);
+		console.log("           ◥◣     ███████████████◣".red);
+		console.log("             ◥◣◢███████████████████◤".red);
+		console.log("                ◥████████████████◤".red);
+		console.log("          █◣  ▌██████████████◣".red);
+		console.log("          ███◣▌█▄▄ ◥███████████◣".red);
+		console.log("          █ ◥█▌█     ◥██████████".red);
+		console.log("          █  ◥▌████    ◥████████◣".red);
+		console.log("               ▇▇◣  ██   ◥███████".red);
+		console.log("               █  █ ██   ◢███████▌".red);
+		console.log("               ███◤ ██ ◢◤   ◥█████◣".red);
+		console.log("               █ ◥◣ ██◤       ◥████".red);
+		console.log("    GOD GOTO SLEEP     ,        ◥██".red);
+		console.log("      "+"GO DE YOUR BUG.".green+"             ◥ ".red);
+	}
 });
-appone.listen(81,'127.0.0.1'); //Setup Port, IP Address!
+ //Setup base Directory!
+ //Setup Your default index, like apache HTTPD.conf file!
